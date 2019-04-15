660d5651f8a4b861afcf587210ba6330afbd4b2c
jeezyb <34031121+jeezyb@users.noreply.github.com>
2019-04-15 19:37:19 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+#crud
+modelo de CRUD em PHP, HTML5, CSS, Javascript, MySQL
+
+#INICIO
+depois de instalar e inicar seu servidor, basta acessar o endereço do seu navegador "http://localhost:8888/crud/"
---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * CRUD Teste para aula de Tópicos II
+ */
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <title>CRUD</title>
+    <link rel="stylesheet" type="text/css" href="styles.css" async>
+    <script src="scripts.js"></script>
+</head>
+
+<body>
+<div id="conteudo">
+    <h2>Modelo CRUD</h2>
+    <div id="msg-php" class="no-display"></div>
+
+    <form method="POST" enctype="multipart/form-data" onSubmit="salvarForm(); return false;" id="frmCrud">
+    <fieldset>
+        <legend>Nome:</legend>
+        <input id="nome" type=text class=input-text required placeholder="Digite seu nome aqui" size=20 name=nome onFocus="inputOn(this)" onBlur="inputOff(this)"/>
+        <legend>Email:</legend>
+        <input id="email" type=email class=input-text required placeholder="Informe seu email" size=30 name=email onFocus="inputOn(this)" onBlur="inputOff(this)"/>
+        <legend>Telefone:</legend>
+        <input id="telefone" type=text class=input-text required pattern="\d*" placeholder="Seu telefone? (apenas números)" size=10 name=telefone onFocus="inputOn(this)" onBlur="inputOff(this)"/>
+    </fieldset>
+    <fieldset>
+        <legend>Foto:</legend>
+        <input type=file id="foto" name=foto class=input-text accept="image/png, image/jpeg"/>
+        <img id="image" class=thumb />
+    </fieldset>
+    <input id="id" type=hidden value="-1" />
+    <input id="nomeFoto" type=hidden value="" />
+    <input type=reset class=button id="btnLimpar" value="Limpar" />
+    <input type=submit class=button id="btnSalvar" value="Salvar" />
+    </form>
+</div>
+
+<div id="lista">
+    <script type="text/javascript">carregarLista();</script>
+</div>
+<p class="rodape">Tópicos Avançados em Computação II <a href="https://www.facebook.com/bruno.dacosta.944">Bruno Maciel </a>!</p>
+</body>
+</html>
